4ebfd647b914640e5c8a6dbf25fd6d92d501ff0b
unknown <Usuario@PFCGSTI3165.PMCG.IMTI>
2020-10-30 08:38:13 -0400

    investing

--- app/Models/Investing/Carteira.php
+++ app/Models/Investing/Carteira.php
@@ -4,7 +4,7 @@
 namespace App\Models\Investing;
 
 use Auth;
-use TCG\Voyager\User;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
