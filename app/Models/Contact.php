01149a9ebbcab100815139b72d8131b7610f9384
unknown <Usuario@PFCGSTI3165.PMCG.IMTI>
2020-11-03 21:29:37 -0400

    contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    protected $table = 'contacts';
+    protected $primaryKey = 'id';
     protected $guarded = [];
     
 }
