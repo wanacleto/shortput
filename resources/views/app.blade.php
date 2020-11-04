005eafe95bd697b3613555238da0f50a36d1371a
unknown <Usuario@PFCGSTI3165.PMCG.IMTI>
2020-11-04 09:10:58 -0400

    contacts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-    <link href="" rel="stylesheet"/>
-    <script src="" defer></script>
-</head>
-<body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 class="app-title">Laravel Inertia Js CRUD</h2>
-        </div>
-    </div>
-</div>
-<hr>
-<div class="container">
-    <div class="row">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        @routes
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
         @inertia
-    </div>
-</div>
-
-</body>
+    </body>
 </html>
