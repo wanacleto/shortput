6aa57748c8a0e872a964f4e92deefa7290562d70
unknown <Usuario@PFCGSTI3165.PMCG.IMTI>
2020-11-03 22:44:33 -0400

    contact

--- resources/js/Pages/Home/Contact.vue
+++ resources/js/Pages/Home/Contact.vue
@@ -1,36 +1,114 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Contact
-            </h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    
-                    <FormRegister />
-                     
-                </div>
-            </div>
-        </div>
-    </app-layout>
+    <div class="col-md-6">
+        <button class="btn btn-sm btn-primary" @click="openModal()">Add New</button>
+        <table class="table table-bordered table-condensed">
+            <thead>
+            <tr>
+                <td>Name</td>
+                <td>Phone</td>
+                <td>Action</td>
+            </tr>
+            </thead>
+            <tr v-for="row in data">
+                <td></td>
+                <td></td>
+                <td width="130">
+                    <button @click="edit(row)" class="btn btn-sm btn-primary">Edit</button>
+                    <button @click="deleteRow(row)" class="btn btn-sm btn-danger">Del</button>
+                </td>
+            </tr>
+        </table>
+
+        <div class="modal fade" id="modal">
+            <div class="modal-dialog">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">New Contact</h4>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input class="form-control" required id="name" v-model="form.name"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input class="form-control" required id="phone" v-model="form.phone"/>
+                        </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" @click="closeModal()">Close</button>
+                        <button type="submit" class="btn btn-primary" v-show="!editMode" @click="save(form)">Save
+                        </button>
+                        <button type="submit" class="btn btn-primary" v-show="editMode" @click="update(form)">Update
+                        </button>
+                    </div>
+                </div><!-- /.modal-content -->
+
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+    </div>
+
+
 </template>
 
 <script>
-    import AppLayout from './../../Layouts/AppLayout'
-    import FormRegister from './FormRegister'
-
     export default {
-      
-        components: {
-            AppLayout,
-            FormRegister,
+        props: ['data'],
+        data() {
+            return {
+                editMode: false,
+                form: {
+                    name: null,
+                    phone: null,
+                },
+            }
         },
+        methods: {
+            openModal: function () {
+                $('#modal').modal('show')
+            },
+            closeModal: function () {
+                $('#modal').modal('hide')
+                this.reset();
+                this.editMode=false;
+            },
+            reset: function () {
+                this.form = {
+                    name: null,
+                    phone: null,
+                }
+            },
+            save: function (data) {
+                this.$inertia.post('/admin/contacts', data)
+                this.reset();
+                this.closeModal();
+                this.editMode = false;
+            },
+            edit: function (data) {
+                this.form = Object.assign({}, data);
+                this.editMode = true;
+                this.openModal();
+            },
+            update: function (data) {
+                if (!confirm('Sure')) return;
+                data._method = 'PUT';
+                this.$inertia.post('/admin/contacts/' + data.id, data)
+                this.reset();
+                this.closeModal();
+            },
+            deleteRow: function (data) {
+                if (!confirm('Sure')) return;
+                data._method = 'DELETE';
+                this.$inertia.post('/admin/contacts/' + data.id, data)
+                this.reset();
+                this.closeModal();
+            }
+        }
     }
 </script>
-
-
-
-
